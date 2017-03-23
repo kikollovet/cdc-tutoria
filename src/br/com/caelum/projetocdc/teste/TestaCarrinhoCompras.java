470af116990034ac5020d1bb7d6664a74a281ae4
Luis <kikollovet@gmail.com>
2017-03-23 15:10:50 -0300

    Nova tarefa CC

---
+++ src/br/com/caelum/projetocdc/teste/TestaCarrinhoCompras.java
@@ -0,0 +1,14 @@
+package br.com.caelum.projetocdc.teste;
+
+public class TestaCarrinhoCompras {
+
+	public static void main(String[] args) {
+		//adicione livros no carrinho
+		//exiba a quantidade de livros no carrinho
+		//exiba o valor total do carrinho
+		//remover livro do carrinho
+		//atualizar a quantidade de determinado livro no carrinho
+		//posso comprar mais de um do mesmo livro
+		//liste todos os livros do carrinho
+	}
+}
