fb35c16bf255fed7fce666c19b8560dcc0c3071b
Luis <kikollovet@gmail.com>
2017-03-23 15:15:32 -0300

    Criei CarrinCompra

---
+++ src/br/com/caelum/projetocdc/CarrinhoDeCompras.java
@@ -0,0 +1,9 @@
+package br.com.caelum.projetocdc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+	private List<Livro> listaLivroCarrinhoCompra = new ArrayList<>();
+}
