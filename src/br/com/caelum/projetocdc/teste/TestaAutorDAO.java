0dc9ab9d288744340d36034ca2f719eeb2f95bcd
kikollovet <kikollovet@gmail.com>
2017-03-29 19:35:52 -0300

    inteface autorDao

--- src/br/com/caelum/projetocdc/teste/TestaAutorDAO.java
+++ src/br/com/caelum/projetocdc/teste/TestaAutorDAO.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import br.com.caelum.projetocdc.Autor;
 import br.com.caelum.projetocdc.dao.AutorBDDao;
+import br.com.caelum.projetocdc.dao.AutorDAO;
 import br.com.caelum.projetocdc.dao.AutorMemoria;
 
 public class TestaAutorDAO {
@@ -12,11 +13,11 @@ public class TestaAutorDAO {
 		Autor autor = new Autor("Alexandre Lourenço");
 		Autor autor2 = new Autor("Joaquim Torres");
 		
-		AutorBDDao dao = new AutorBDDao(); 
+		AutorDAO dao = new AutorBDDao(); 
 		dao.adiciona(autor);
 		dao.adiciona(autor2);
 		
-		
+		System.out.println("adicionado");
 		//dao.altera(autor, 2);
 	}
 }
