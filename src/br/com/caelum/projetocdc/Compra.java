87219ed7281d6a21067d7a1b624a4540907e2312
kikollovet <kikollovet@gmail.com>
2017-04-04 19:17:49 -0300

    Classe Compra

---
+++ src/br/com/caelum/projetocdc/Compra.java
@@ -0,0 +1,38 @@
+package br.com.caelum.projetocdc;
+
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Compra {
+
+	private int id;
+	private Usuario usuario;
+	private Calendar data;
+	private Set<Item> itens = new HashSet<>();
+	
+	public Compra(Usuario usuario, Calendar data) {
+		this.usuario = usuario;
+		this.data = data;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public Set<Item> getItens() {
+		return itens;
+	}
+}
